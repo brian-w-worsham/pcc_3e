a3796ab3023c0c7fe5a40c9266f4e2188b0ac25c
Brian Worsham <Brian.Worsham@dell.com>
2025-10-30 10:08:12 -0500

    Exercises 10-6 though 10-8

--- chapter_10/try_it_yourself_exercises/try_it_yourself_ch_10.py
+++ chapter_10/try_it_yourself_exercises/try_it_yourself_ch_10.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 # 10-1. Learning Python: Open a blank file in your text editor and write a few
 # lines summarizing what you have learned about Python so far. Start each line
 # with the phrase In Python you can... Save the file as learning_python.txt in
@@ -72,3 +73,55 @@ while True:
 
 guest_book_path.write_text("\n".join(guest_names), encoding="utf-8")
 print(f"Guest book has been written to {guest_book_path}")
+
+
+# 10-6. Addition: One common problem when prompting for numerical input occurs
+# when people provide text instead of numbers. When you try to convert the
+# input to an int, you’ll get a ValueError. Write a program that prompts for
+# two numbers. Add them together and print the result. Catch the ValueError if
+# either input value is not a number, and print a friendly error message. Test
+# your program by entering two numbers and then by entering some text instead
+# of a number.
+
+keep_going = True
+
+while keep_going:
+    try:
+        x = input("Give me a number: ")
+        x = int(x)
+
+        y = input("Give me another number: ")
+        y = int(y)
+    except ValueError:
+        print("Sorry, I really needed a number.")
+    else:
+        sum = x + y
+        print(f"The sum of {x} and {y} is {sum}.")
+        keep_going = False
+
+# 10-8. Cats and Dogs: Make two files, cats.txt and dogs.txt. Store at least
+# three names of cats in the first file and three names of dogs in the second
+# file. Write a program that tries to read these files and print the contents
+# of the files to the screen. Wrap each name in a try-except block to catch the
+# FileNotFoundError, and print a friendly message if a file is missing. Move
+# one of the files to a different location on your system, and make sure the
+# code in the except block executes properly.
+
+
+def read_file(filename):
+    """Read and print the contents of a file."""
+    try:
+        with open(filename) as file:
+            contents = file.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
+    else:
+        print(contents)
+
+
+filenames = ["cats.txt", "dogs.txt", "missing.txt"]
+
+for filename in filenames:
+    relative_filename = base_dir / filename
+    print(f"\nReading file: {relative_filename}")
+    read_file(relative_filename)
