912f48467f37a859fc3635723e14652f8878e2aa
Brian Worsham <Brian.Worsham@dell.com>
2025-11-01 13:55:48 -0500

    finished chapter 11 tests

---
+++ chapter_11/try_it_yourself_exercises/employee.py
@@ -0,0 +1,16 @@
+"""Employee class."""
+
+
+class Employee:
+    ''' A class to represent an employee. '''
+
+    def __init__(self, first_name, last_name, salary):
+        ''' Initialize the employee. '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+
+    def give_raise(self, amount=5000):
+        ''' Give the employee a raise. '''
+        self.salary += amount
+
