9fd0d80b31142db72caff837bb1f12985152dcf5
Brian Worsham <Brian.Worsham@dell.com>
2025-12-04 10:41:43 -0600

    in progress

--- chapter_12/alien_invasion/alien_invasion.py
+++ chapter_12/alien_invasion/alien_invasion.py
@@ -37,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             # 60 FPS
             self.clock.tick(60)
@@ -121,6 +122,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
 
 if __name__ == "__main__":
