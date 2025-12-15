02525e3b271ac73ce3b04b169e1e40a37eea3fd5
Brian Worsham <Brian.Worsham@dell.com>
2025-12-15 14:24:04 -0600

    adjusting settings

--- chapter_12/alien_invasion/alien_invasion.py
+++ chapter_12/alien_invasion/alien_invasion.py
@@ -60,7 +60,20 @@ class AlienInvasion:
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
-    def _create_fleet(self):
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+    def _create_fleet(self):  
         """Create the fleet of aliens."""
         # Create an alien and keep adding aliens until there is no room left.
         # Spacing between aliens is one alien width and one alien height.
@@ -123,8 +136,23 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
+        """Check if the fleet is at an edge, then update positions of all aliens in the fleet."""
+        self._check_fleet_edges()
         self.aliens.update()
 
 
